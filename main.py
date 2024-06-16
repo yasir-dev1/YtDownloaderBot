02ef75a926cccc22483208c86ec073cf637dd546
yasir-dev1 <yasireposta3@gmail.com>
2024-06-16 21:39:49 +0300

    Modified to run on AWS EC2

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+#!./venv/bin/python3
 import os
 import re
 import telebot
 import  time
+import dotenv
 from pytube import YouTube
 
-BOT_TOKEN = os.environ.get('TOKEN')
+dotenv.load_dotenv()
+
+
+BOT_TOKEN = os.getenv('TOKEN')
 
 bot = telebot.TeleBot(BOT_TOKEN)
 url_regex = r"^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$"
@@ -13,20 +18,23 @@ url_regex = r"^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$"
 def send_welcome(message):
     bot.reply_to(message, "Please Enter YouTube Video URL to Download")
 
-#TODO: Fix  Time  Error 
-
 @bot.message_handler(func=lambda msg: True)
 def Download(message) :
     if re.match(url_regex, message.text):
-        video_url = message.text
-        yt = YouTube(video_url)
-        stream = yt.streams.filter(progressive=True,).order_by('resolution').desc().first()
-        video_path = str(round(time.time() * 1000))+".mp4"
-        stream.download(filename=video_path)
-        video = open(video_path,'rb+')
-        bot.send_video(chat_id=message.chat.id, video=video)
-
-        
+        try:
+            video_url = message.text
+            yt = YouTube(video_url)
+            stream = yt.streams.filter(progressive=True,).order_by('resolution').desc().first()
+            video_path = str(round(time.time() * 1000))+".mp4"
+            stream.download(filename=video_path)
+            video = open(video_path,'rb+')
+            bot.send_video(chat_id=message.chat.id, video=video)
+            video.close()
+            os.remove(video_path)
+        except:
+            bot.reply_to(message, "Bazı Sıkıntılar var lütfen tekrar denyeiyiniz eğer 2. kere bu  uyarıyı görüyorsan birdaha bu videoyu deneme bir sorun var demeki")
+            video.close()
+            os.remove(video_path)
     else:
         bot.reply_to(message, "Please enter a valid YouTube video URL.")
 
